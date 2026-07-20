ba8043bc4989eed93bb98a27f51a99d216d1375b
Mikaele <mikaelethainara@gmail.com>
2026-07-20 16:08:11 -0300

    commit inicial

---
+++ src/main/java/com/mikaele/api_simple_transfer/ApiSimpleTransferApplication.java
@@ -0,0 +1,13 @@
+package com.mikaele.api_simple_transfer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSimpleTransferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSimpleTransferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikaele/api_simple_transfer/ApiSimpleTransferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikaele.api_simple_transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSimpleTransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
